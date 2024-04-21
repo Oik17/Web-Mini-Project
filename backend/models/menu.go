f16d962d184b9defdb4e6a6a7513e60bd91f3f47
Akshat Gupta <akshat.g1707@gmail.com>
2024-04-21 16:01:36 +0530

    fix: frontend

--- backend/models/menu.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Menu struct {
-	ID          uint    `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float64 `json:"price"`
-}
